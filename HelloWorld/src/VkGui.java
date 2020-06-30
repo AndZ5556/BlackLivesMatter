5907744d740ff5d005a61d460d16d7b09264f108
Andrew Bykov <42899010+AndZ5556@users.noreply.github.com>
2020-06-30 21:46:56 +0300

    Add info window

--- HelloWorld/src/VkGui.java
+++ HelloWorld/src/VkGui.java
@@ -10,6 +10,7 @@ import javax.swing.*;
 
 public class VkGui extends JFrame {
     private final JButton buildButton = new JButton("Build Graph");
+    private final JButton infoButton = new JButton("INFO");
     private final JButton addButton = new JButton("Add user");
     private final JTextArea input = new JTextArea();
     private final JLabel label1 = new JLabel("Input all users'");
@@ -21,16 +22,57 @@ public class VkGui extends JFrame {
     private final JRadioButton radioButton2 = new JRadioButton("From keyboard");
     private final JPanel infoPanel = new JPanel();
     private final JLayeredPane layeredPane = new JLayeredPane();
+    private final UserFields userFields = new UserFields();
     private GraphPanel panel = new GraphPanel();
     private JScrollPane scrollPane = new JScrollPane();
     private JScrollPane scrollInfoText = new JScrollPane();
-    private final UserFields userFields = new UserFields();
+
+
     JLabel picLabel = new JLabel();
     private Graph graph = new Graph();
     private String inputString;
-    private DragImage dragImage = new DragImage();
+    private final DragImage dragImage = new DragImage();
 
+    static class HelpPanel extends JFrame{
+        HelpPanel(){
+            super("INFO");
+            this.setBounds(700,200, 500, 700);
+            this.setVisible(true);
+            this.setLayout(null);
+            JTextArea info = new JTextArea();
+            JLabel picture = new JLabel();
+            info.setText("\tHello, this is high level program made by Luxury Genius Black Team " +
+                    "subdivision of Black Lives Matter movement.\n\n" +
+                    "It is our statement and direct message to everyone who is against our rights being heard.\n\n" +
+                    "Main purpose of this IT product is to show all naive people how Social Networks really affect their lives. " +
+                    "Simple graph visualisation can demonstrate your whole being.\n" +
+                    "If you are ready to touch your fragile little inner world, read the instruction below.\n\n" +
+                    "1)Choose input method(from file or from keyboard)\n" +
+                    "2)Input data about Social Network users in such form" +
+                    "\tNAME SURNAME AGE: FRIEND1_NAME FRIEND1_SURNAME, FRIEND2_NAME ...\n" +
+                    "3)Press button BUILD GRAPH\n" +
+                    "4)If you forgot somebody, you can add him or her by using field in right upper corner\n" +
+                    "5)Now you can see nearly everything but if you want to go further press PAINT OSTOV\n" +
+                    "6)Your life has changed forever");
+            info.setLineWrap(true);
+            info.setWrapStyleWord(true);
+            info.setFont(new Font("Dialog", Font.PLAIN, 14));
+            JScrollPane scrollPane = new JScrollPane(info, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            scrollPane.setBounds(0, 0, 485, 300);
 
+            try {
+                BufferedImage myPicture = ImageIO.read(new File("Power.png"));
+                Image image = myPicture.getScaledInstance(400,400, 300);
+                picture = new JLabel(new ImageIcon(image));
+                picture.setBounds(45,300, 400, 400);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            this.getLayeredPane().add(picture, JLayeredPane.POPUP_LAYER);
+            this.getLayeredPane().add(scrollPane, JLayeredPane.POPUP_LAYER);
+            this.setAlwaysOnTop(true);
+        }
+    }
     static class DragImage{
         public int x;
         public int y;
@@ -46,14 +88,12 @@ public class VkGui extends JFrame {
         private final JTextField age = new JTextField();
         private final JTextField friends = new JTextField();
     }
-
-
     public VkGui(){
         super("VK visualisation");
         this.setBounds(100, 100, 1430, 850);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setAllForms();
-
+        new HelpPanel();
         Container container = this.getContentPane();
         container.setLayout(null);
         container.add(radioButton1);
@@ -74,6 +114,7 @@ public class VkGui extends JFrame {
         container.add(userFields.agel);
         container.add(userFields.friendsl);
         container.add(addButton);
+        container.add(infoButton);
         container.add(scrollPane);
         container.add(layeredPane);
     }
@@ -81,20 +122,13 @@ public class VkGui extends JFrame {
         public void mouseDragged(MouseEvent e){
             int dx = dragImage.x - e.getLocationOnScreen().x;
             int dy = dragImage.y - e.getLocationOnScreen().y;
-
-            System.out.println(dx);
-            System.out.println(dy);
             scrollPane.getHorizontalScrollBar().setValue(dx);
             scrollPane.getVerticalScrollBar().setValue(dy);
-
-            //scrollPane.getHorizontalScrollBar().setValue(dragImage.x - e.getX());
         }
         public void mouseMoved(MouseEvent e){
 
         }
-
     }
-
     class mouseClickListener implements MouseListener {
         @Override
         public void mouseClicked(MouseEvent e) {
@@ -136,7 +170,8 @@ public class VkGui extends JFrame {
                 picLabel.setBounds(0,110, 300, 20);
             }
 
-            StringBuilder text = new StringBuilder("Пользователь: " + user.getName() + " " + user.getSurname() + "\nВозраст: " + user.getAge() + "\nДрузья: ");
+            StringBuilder text = new StringBuilder("Пользователь: " + user.getName()
+                    + " " + user.getSurname() + "\nВозраст: " + user.getAge() + "\nДрузья: ");
             for(User user1 : user.friends){
                 text.append(user1.getName());
                 text.append(" ");
@@ -168,22 +203,20 @@ public class VkGui extends JFrame {
         public double zoomFactor = 1;
         public boolean zoomed = true;
         public AffineTransform at;
-
         Graphics2D g2;
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
             panel.setPreferredSize(new Dimension((int)(graph.size * 3 * panel.zoomFactor),
-                    (int)(graph.size * 3 * panel.zoomFactor)));
+                    (int)(graph.size * 3 * panel.zoomFactor) + 500));
             g2=(Graphics2D)g;
             if (zoomed) {
+                scrollPane.getVerticalScrollBar().setValue(0);
+                scrollPane.getHorizontalScrollBar().setValue(0);
                 at = g2.getTransform();
                 at.scale(zoomFactor, zoomFactor);
                 zoomed = false;
             }
-
-            //System.out.println(at + "!");
             g2.transform(at);
-
             g2.setColor(Color.BLACK);
             for(User user : graph.users.users){
                 user.drawUser(g2);
@@ -312,6 +345,7 @@ public class VkGui extends JFrame {
         SwingUtilities.updateComponentTreeUI(this);
     }
     public void setAllForms(){
+        infoButton.setBounds(1300, 5, 100, 50);
         label1.setBounds(25,45,80,10);
         label2.setBounds(25,57,80,10);
         label3.setBounds(1020,20,200,15);
@@ -341,6 +375,7 @@ public class VkGui extends JFrame {
             String age = userFields.age.getText();
             addUser(name + " " + surname + " " + age + ": " + friends);
         });
+        infoButton.addActionListener(e -> new HelpPanel());
         checkBoxOstov.addActionListener(e -> paintGraph());
         checkBoxNonFriends.addActionListener(e -> paintGraph());
         radioButton1.addActionListener(e -> {
